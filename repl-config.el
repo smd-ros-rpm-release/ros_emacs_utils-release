3c6534070851cc289ea3f39ded52074561cab713
Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
2014-09-26 21:35:41 +0200

    Rebase from 'debian/indigo/roslisp_repl'

--- repl-config.el
+++ repl-config.el
@@ -8,18 +8,12 @@
 (show-paren-mode 1)
 (global-set-key '[delete] 'delete-char)
 (setq minibuffer-max-depth nil)
-(setq auto-mode-alist
-      (append '(("\\.C$"       . c++-mode)
-                ("\\.cc$"      . c++-mode)
-                ("\\.c$"       . c-mode)
-                ("\\.h$"       . c++-mode)
-                ("makefile$"   . makefile-mode)
-                ("Makefile$"   . makefile-mode)
-                ("\\.asd"      . lisp-mode)
-                ("\\.launch"   . xml-mode)) auto-mode-alist))
 (autoload 'mwheel-install "mwheel" "Enable mouse wheel support.")
 
 ;;; Start slime
+;; ``slime-config`` is located in the ``slime_ros`` package.
+;; It's path is passed to emacs through the -L argument of
+;; the ``roslisp_repl`` executable.
 (require 'slime-config)
 (slime)
 
